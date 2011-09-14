705f87f4764bc111afbcca2bbd017d536bba645b
Will Brown <will.h.brown@gmail.com>
2011-09-13 23:29:48 -0400

    Update name in run script

--- runsim.sh
+++ runsim.sh
@@ -2,6 +2,6 @@
 
 CLASSPATH=$(ls sim/*.jar | xargs | sed 's/ /:/g'):$(ls iSnorkeling/lib/*.jar | xargs | sed 's/ /:/g')
 SOURCEPATH=iSnorkeling/src/:sim/src/
-javac -cp $CLASSPATH -sourcepath $SOURCEPATH sim/src/isnork/sim/GameEngine.java iSnorkeling/src/isnork/g3/AwsmPlayer.java -d bin/
+javac -cp $CLASSPATH -sourcepath $SOURCEPATH sim/src/isnork/sim/GameEngine.java iSnorkeling/src/isnork/g3/AwwsimPlayer.java -d bin/
 java -cp $CLASSPATH:bin/ isnork.sim.GameEngine gui
 
