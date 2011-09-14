3a45e1829784792a87e04eab2894a3fd0cb93a93
Will Brown <will.h.brown@gmail.com>
2011-09-13 23:53:22 -0400

    Update Awwsim to do a random walk

--- iSnorkeling/src/isnork/g3/AwwsimPlayer.java
+++ iSnorkeling/src/isnork/g3/AwwsimPlayer.java
@@ -10,6 +10,7 @@ import isnork.sim.iSnorkMessage;
 
 
 public class AwwsimPlayer extends Player{
+  private static final int MAX_TICKS_PER_ROUND = 60 * 8;
 	
 	String name;
 	private Direction nextMove;
@@ -18,6 +19,8 @@ public class AwwsimPlayer extends Player{
 	private Set<SeaLifePrototype> species;
 	private int penalty;
 	private Cartogram carto;
+  private int ticks = 0;
+  private Point2D currentLocation;
 
 	@Override
 	public String getName() {
@@ -47,6 +50,8 @@ public class AwwsimPlayer extends Player{
 	public String tick(Point2D myPosition, Set<Observation> whatYouSee,
 			Set<iSnorkMessage> incomingMessages,
 			Set<Observation> playerLocations) {
+    ticks++;
+    currentLocation = myPosition;
 		carto.update(myPosition,whatYouSee,playerLocations,incomingMessages); 
 		carto.getNextDirection();
 		return carto.getMessage();
@@ -57,7 +62,44 @@ public class AwwsimPlayer extends Player{
 	 */
 	@Override
 	public Direction getMove() {
-		return nextMove;
-	}
+    if (ticks < MAX_TICKS_PER_ROUND - 100) {
+      Direction[] dirs = Direction.values();
+      return dirs[random.nextInt(dirs.length)];
+    } else {
+      // Move towards boat
+      int deltaX = 0;
+      int deltaY = 0;
+      if (currentLocation.getX() < 0)
+        deltaX = 1;
+      else if (currentLocation.getX() > 0)
+        deltaX = -1;
 
+      if (currentLocation.getY() < 0)
+        deltaY = 1;
+      else if (currentLocation.getY() > 0)
+        deltaY = -1;
+
+      if (deltaX > 0 && deltaY == 0) {
+        return Direction.E;
+      } else if (deltaX > 0 && deltaY > 0) {
+        return Direction.NE;
+      } else if (deltaX > 0 && deltaY < 0) {
+        return Direction.SE;
+      } else if (deltaX == 0 && deltaY == 0) {
+        return Direction.STAYPUT;
+      } else if (deltaX == 0 && deltaY < 0) {
+        return Direction.N;
+      } else if (deltaX == 0 && deltaY > 0) {
+        return Direction.S;
+      } else if (deltaX < 0 && deltaY == 0) {
+        return Direction.W;
+      } else if (deltaX < 0 && deltaY > 0) {
+        return Direction.NW;
+      } else if (deltaX < 0 && deltaY < 0) {
+        return Direction.NE;
+      } else {
+        throw new RuntimeException("I HAVE NO IDEA");
+      }
+    }
+	}
 }
