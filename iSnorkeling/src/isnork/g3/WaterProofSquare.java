44f4b74c62f1c81df05c1b18306bd061dadeae81
Hans E Hyttinen <hans@lumilux.org>
2011-09-13 23:43:44 -0400

    implement getters and setters for WPS

--- iSnorkeling/src/isnork/g3/WaterProofSquare.java
+++ iSnorkeling/src/isnork/g3/WaterProofSquare.java
@@ -5,15 +5,45 @@ import isnork.sim.Player;
 import java.util.Set;
 import com.google.common.collect.*;
 
+/**
+ * Records what creatures, divers, the danger, and happiness of a square
+ */
 public class WaterProofSquare extends AbstractSquare{
 	private Set<SeaLife> creatures = Sets.newHashSet();
 	private Set<Player> divers = Sets.newHashSet();
+	private double danger = 0.0;
+	private double happiness = 0.0;
 
 	void addCreature(SeaLife creature) {
 		creatures.add(creature);
 	}
 
+	Set<SeaLife> getCreatures() {
+		return creatures;
+	}
+
 	void addDiver(Player diver) {
 		divers.add(diver);
 	}
+
+	Set<Player> getDivers() {
+		return divers;
+	}
+
+	void setDanger(double danger) {
+		this.danger = danger;
+	}
+
+	double getDanger() {
+		return danger;
+	}
+
+	void setHappiness(double happiness) {
+		this.happiness = happiness;
+	}
+
+	double getHappiness() {
+		return happiness;
+	}
+
 }
