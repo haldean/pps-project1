fea4950d839d37413e6aa2aa814d88be87edf859
Hans E Hyttinen <hans@lumilux.org>
2011-09-14 00:19:08 -0400

    updated Square with getters and setters

--- iSnorkeling/src/isnork/g3/Square.java
+++ iSnorkeling/src/isnork/g3/Square.java
@@ -1,5 +1,12 @@
 package isnork.g3;
 
 public interface Square {
-	
+	void addCreature(SeaLife creature);
+	Set<SeaLife> getCreatures();
+	void addDiver(Player diver);
+	Set<Player> getDivers();
+	void setDanger(double danger);
+	double getDanger();
+	void setHappiness(double happiness);
+	double getHappiness();
 }
