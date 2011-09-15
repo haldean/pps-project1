a54a73a253943ce13853f00af7da388678998999
Will Brown <will.h.brown@gmail.com>
2011-09-15 12:35:06 -0400

    Fixed jUnit import, made warnings errors

--- runsim.sh
+++ runsim.sh
@@ -2,6 +2,9 @@
 
 CLASSPATH=$(ls sim/*.jar | xargs | sed 's/ /:/g'):$(ls iSnorkeling/lib/*.jar | xargs | sed 's/ /:/g')
 SOURCEPATH=iSnorkeling/src/:sim/src/
-javac -cp $CLASSPATH -sourcepath $SOURCEPATH sim/src/isnork/sim/GameEngine.java iSnorkeling/src/isnork/g3/AwwsimPlayer.java -d bin/
-java -cp $CLASSPATH:bin/ isnork.sim.GameEngine gui
+javac -cp $CLASSPATH -sourcepath $SOURCEPATH -Werror -Xlint:all \
+  sim/src/isnork/sim/GameEngine.java \
+  iSnorkeling/src/isnork/g3/AwwsimPlayer.java \
+  -d bin/ && \
+  java -cp $CLASSPATH:bin/ isnork.sim.GameEngine gui
 
